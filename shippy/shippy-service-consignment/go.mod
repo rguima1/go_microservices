9d7dfabd083627801a664db7dea3ba41b9ea948b
Renato Rosa Guimarães <rnr.guimaraes@gmail.com>
2020-10-08 14:09:05 -0300

    Initial commit for shippy-service-consignment

---
+++ shippy/shippy-service-consignment/go.mod
@@ -0,0 +1,15 @@
+module github.com/rguima1/go_microservices/shippy/shippy-service-consignment
+
+go 1.15
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/micro/go-micro/v2 v2.9.1
+	golang.org/x/net v0.0.0-20201006153459-a7d1128ccaa0 // indirect
+	golang.org/x/sys v0.0.0-20201008064518-c1f3e3309c71 // indirect
+	google.golang.org/genproto v0.0.0-20201007142714-5c0e72c5e71e // indirect
+	google.golang.org/grpc v1.32.0 // indirect
+	google.golang.org/protobuf v1.25.0
+)
---
+++ shippy/shippy-service-consignment/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/micro/go-micro/v2"
+	pb "github.com/rguima1/go_microservices/shippy/shippy-service-consignment/proto/consignment"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type Repository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+type consignmentService struct {
+	repo repository
+}
+
+func (s *consignmentService) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *consignmentService) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	service := micro.NewService(micro.Name("shippy.service.consignment"))
+
+	service.Init()
+
+	if err := pb.RegisterShippingServiceHandler(service.Server(), &consignmentService{repo}); err != nil {
+		log.Panic(err)
+	}
+
+	if err := service.Run(); err != nil {
+		log.Panic(err)
+	}
+}
